6bbbbf8ca0162ea8649d711c53008618e4048542
laura <you@example.com>
2024-08-21 12:15:25 -0500

    hi

---
+++ problem2/src/problem2/Problem2.java
@@ -0,0 +1,16 @@
+package problem2;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		// For loop
+		
+		
+		for ( int x = 1; x<=50; x++ ){
+		System.out.println(x);
+		
+		}
+
+	}
+
+}
